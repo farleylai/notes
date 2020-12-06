36c9e50a4eef6b13e4d4235502be6c0f1c44257a
farleylai <farleylai@users.noreply.github.com>
2020-12-06 07:39:20 +0000

    deploy: f59c6fc737e80299e54b1776c68fcc7218767472

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/notes/assets/js/search-data.json', true);
+    request.open('GET', '/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
